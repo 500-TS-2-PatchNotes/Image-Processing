04ba141899a39efbdd9079b424b242eb64507f89
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2025-03-03 16:20:21 -0700

    Fixed some prints

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from pandas import DataFrame
 
 from lib.ColourExtractor import *
 from lib.data2img import *
@@ -7,9 +8,12 @@ def main():
     img_loc = input("Enter the location of the image: ")
     extractor = ColourExtractor(img_loc)
     
+    print(f"Dominant Colors: {extractor.get_dominant_colours()}".replace(", np.", " ").replace("(np.", "(").replace("uint8(", "").replace(") ", ", ").replace("))", ")"))
+    print(f"Average Color: {extractor.get_average_colour()}".replace(", np.", " ").replace("(np.", "(").replace("int64(", "").replace(") ", ", ").replace("))", ")"))
+
     df = extractor.toDF()
 
-    df.head()
+    print(df.head())
 
 if __name__ == "__main__":
     main()
