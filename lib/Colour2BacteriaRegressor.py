77dead984887145c79e9a4c1eb8ec4e5d498c8f6
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2025-03-14 14:40:26 -0600

    Implemented Regressor

--- lib/Colour2BacteriaRegressor.py
+++ lib/Colour2BacteriaRegressor.py
@@ -8,4 +8,9 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 class Regressor:
-    
+    def train(self, X_train, y_train):
+        self.regressor = RandomForestRegressor(n_estimators=3, random_state=0)
+        self.regressor.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.regressor.predict(X_test)
